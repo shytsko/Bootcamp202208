98edc29ee1f1b1285bd661bfaa116c7f96593450
Vladimir Shytsko <shytsko.v@gmail.com>
2022-08-29 16:38:02 +0300

    Add C# client

---
+++ CS/Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+var client = new TcpClient("127.0.0.1", 33333);
+var sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);
+var sReader = new StreamReader(client.GetStream(), Encoding.UTF8);
+
+
+
+
+while (true)
+{
+    Console.Write("> ");
+    string message = Console.ReadLine();
+    sWriter.WriteLine(message);
+    sWriter.Flush();
+}
